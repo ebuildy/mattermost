24546778926f54ee61f3c3464b80c206b4b5fadd
ayush-chauhan233 <ayush.chauhan@brightscout.com>
2024-12-10 15:49:49 +0530

    [MA-15]: Removed the unnecessary dialog text from toggle button's aria-label property

--- webapp/channels/src/components/user_settings/security/user_settings_security.tsx
+++ webapp/channels/src/components/user_settings/security/user_settings_security.tsx
@@ -1041,6 +1041,7 @@ export class SecurityTab extends React.PureComponent<Props, State> {
                                 id: 'user.settings.security.viewHistory.icon',
                                 defaultMessage: 'Access History Icon',
                             })}
+                            aria-hidden='true'
                         />
                         <FormattedMessage
                             id='user.settings.security.viewHistory'
@@ -1059,6 +1060,7 @@ export class SecurityTab extends React.PureComponent<Props, State> {
                                 id: 'user.settings.security.logoutActiveSessions.icon',
                                 defaultMessage: 'Active Sessions Icon',
                             })}
+                            aria-hidden='true'
                         />
                         <FormattedMessage
                             id='user.settings.security.logoutActiveSessions'
